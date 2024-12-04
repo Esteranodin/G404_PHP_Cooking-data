feba8be90cc73da2048e65da5104387e963136e4
esteranodin <esteranodin@hotmail.fr>
2024-12-04 21:36:25 +0100

    ADD: liste_films

--- Dictionnaire/index.php
+++ Dictionnaire/index.php
@@ -42,3 +42,5 @@ $dico = explode("\n", $string);
     
 // }
 // echo $wordQ;
+
+?>
---
+++ Liste de films/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// top100 des films visionnés au États-Unis sur la plateforme iTunes.
+
+$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
+$brut = json_decode($string, true);
+$top = $brut["feed"]["entry"]; # liste de films
+
+// json_decode() permet de décoder une chaîne JSON (comme celle lue depuis le fichier) et de la convertir en un tableau ou un objet. Le premier argument est la chaîne JSON à décoder (ici $string).
+// Le deuxième argument (ici, true) indique que le résultat doit être un tableau associatif au lieu d'un objet. Si on avait passé false, le résultat aurait été un objet.
+
+// La variable $brut = tableau où les clés sont les éléments de l'objet JSON.
+//  La variable $top = un tableau de la liste.
+// ==> clé principale = feed et sous-clé = entry
+
+// echo "<pre>";
+// print_r($top); 
+// echo "</pre>";
+
+echo $top['label'];
+
+?>
